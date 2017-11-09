ae1f39676595cbab33ab9110f1400e986810ba06
luisaGonzales <luisagc711@gmail.com>
2017-11-09 05:15:55 -0500

    Store

--- src/App.js
+++ src/App.js
@@ -4,21 +4,11 @@ import {connect} from 'redux-zero/react';
 import {Name} from './Name'
 
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = ({personal, info, socials, repos}) => {
+  return (
+    <Name name={personal.name}/>
+  );
 }
 
-const mapToProps = ({}) => ({});
+const mapToProps = ({personal, info, socials, repos}) => ({personal, info, socials, repos});
 export default connect (mapToProps)(App);
---
+++ src/Name.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export const Name = ({name}) => {
+    return (
+        <div>{name}</div>
+    )
+}
