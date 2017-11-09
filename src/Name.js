df2062e3ef0a25820253964c50f4cf9f67de8993
luisaGonzales <luisagc711@gmail.com>
2017-11-09 05:48:51 -0500

    Name component

--- src/Name.js
+++ src/Name.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import './Name.css';
+import {Col, Grid, Row} from 'react-bootstrap';
 
 export const Name = ({name}) => {
     return (
-        <div>{name}</div>
+        <header className='welcome'>
+            <Grid className='description'>
+                <Row>
+                    <Col sm={12}>
+                        <h1 className='text1'>{name}</h1>
+                        <p><a className="btn btn-primary btn-header" role="button">CONÓCEME</a></p>
+                    </Col>
+                </Row>
+            </Grid>
+        </header>
     )
 }
