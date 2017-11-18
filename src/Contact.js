be6e19f333d6e54e5ffd37e6dfba7c36bb349db9
luisaGonzales <luisagc711@gmail.com>
2017-11-17 20:09:35 -0500

    Agregando contact

--- src/Contact.js
+++ src/Contact.js
@@ -8,10 +8,29 @@ export const Contact = ({title}) => {
             <Grid>
                 <Row>
                     <Col sm={12}>
-                        <h1 className="contact__title">{title}</h1>  
+                        <h1 className="contact__title">{title}</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col sm={12}>
+                        <form id="formulario">
+                            <hr/>
+                            <div className="dato">Name</div>
+                            <input type="text" className="info" id="name" required=""/>
+                            <hr/>
+                            <div className="dato">Email Address</div>
+                            <input type="email" className="info" id="email" required=""/>
+                            <hr/>
+                            <div className="dato">Phone Number</div>
+                            <input type="number" className="info" maxlength="9" id="number" required=""/>
+                            <hr/>
+                            <div className="dato">Message</div><input type="text" className="info" id="message" required=""/>
+                            <hr/>
+                            <input type="submit" value="Send" className="btnSend" />    
+                        </form>
                     </Col>
                 </Row>
             </Grid>
         </div>
-    );
+    )
 }
